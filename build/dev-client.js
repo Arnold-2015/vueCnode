3dcf9ddde297d6b29752819aafd228d0ab198a6d
Arnold-keyx <arnold2015@sina.com>
2017-04-24 12:58:10 +0800

    update

--- build/dev-client.js
+++ build/dev-client.js
@@ -1,8 +1,9 @@
-var hotClient = require('webpack-hot-middleware/client')
+/* eslint-disable */
+require('eventsource-polyfill')
+var hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
-// 订阅事件，当 event.action === 'reload' 时执行页面刷新
 hotClient.subscribe(function (event) {
-    if (event.action === 'reload') {
-        window.location.reload()
-    }
+  if (event.action === 'reload') {
+    window.location.reload()
+  }
 })
--- build/dev-server.js
+++ build/dev-server.js
@@ -1,44 +1,71 @@
-// 引入必要的模块
+require('./check-versions')()
+var config = require('../config')
+if (!process.env.NODE_ENV) process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+var path = require('path')
 var express = require('express')
 var webpack = require('webpack')
-var config = require('./webpack.dev.config')
+var opn = require('opn')
+var proxyMiddleware = require('http-proxy-middleware')
+var webpackConfig = require('./webpack.dev.conf')
 
-// 创建一个express实例
+// default port where dev server listens for incoming traffic
+var port = process.env.PORT || config.dev.port
+// Define HTTP proxies to your custom API backend
+// https://github.com/chimurai/http-proxy-middleware
+var proxyTable = config.dev.proxyTable
+
 var app = express()
+var compiler = webpack(webpackConfig)
 
-// 调用webpack并把配置传递过去
-var compiler = webpack(config)
-
-// 使用 webpack-dev-middleware 中间件
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-    publicPath: config.output.publicPath,
-    stats: {
-        colors: true,
-        chunks: false
-    }
+  publicPath: webpackConfig.output.publicPath,
+  stats: {
+    colors: true,
+    chunks: false
+  }
 })
 
-// 使用 webpack-hot-middleware 中间件
 var hotMiddleware = require('webpack-hot-middleware')(compiler)
-
-// webpack插件，监听html文件改变事件
+// force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
-    compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
-        // 发布事件
-        hotMiddleware.publish({ action: 'reload' })
-        cb()
-    })
+  compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
+    hotMiddleware.publish({ action: 'reload' })
+    cb()
+  })
 })
 
-// 注册中间件
+// proxy api requests
+Object.keys(proxyTable).forEach(function (context) {
+  var options = proxyTable[context]
+  if (typeof options === 'string') {
+    options = { target: options, changeOrigin: true }
+  }
+  app.use(proxyMiddleware(context, options))
+})
+// handle fallback for HTML5 history API
+app.use(require('connect-history-api-fallback')())
+
+// serve webpack bundle output
 app.use(devMiddleware)
-// 注册中间件
+
+// enable hot-reload and state-preserving
+// compilation error display
 app.use(hotMiddleware)
-// 监听 8888端口，开启服务器
-app.listen(8888, function (err) {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log('Listening at http://localhost:8888')
+
+// serve pure static assets
+var staticPath = path.posix.join(config.build.assetsPublicPath, config.build.assetsSubDirectory)
+app.use(staticPath, express.static('./public'))
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  var uri = 'http://localhost:' + port
+  console.log('Listening at ' + uri + '\n')
+
+  // when env is testing, don't need open it
+  if (process.env.NODE_ENV !== 'testing') {
+    opn(uri)
+  }
 })
